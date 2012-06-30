51f4a29f340d5d6bd71c0c1eeaa23961c2ed334f
Viet La <vietdeveloper@live.com>
2012-06-30 01:09:52 -0700

    Second commit

---
+++ HelloWorldGit/src/Hello.java
@@ -0,0 +1,10 @@
+/*
+ * 06/20/2012: Exercise using Git with Eclipse 4.2 (Juno)
+ * based on http://wiki.eclipse.org/EGit/User_Guide
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello Git World!");
+	}
+
+}
